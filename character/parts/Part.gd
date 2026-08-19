ac378afa7f8e8d251aa138355478a74c0ac99ae8
Kai R <stfourrtup@gmail.com>
2026-08-19 19:25:32 -0400

    refactor: inputs for actions are decided by parts now

--- character/parts/Part.gd
+++ character/parts/Part.gd
@@ -2,24 +2,36 @@ extends Node2D
 class_name Part
 
 var character: RobotCharacter
+
+# == charge
 var charge: int = charge_capacity
 var charge_capacity: int = 100
+signal charge_changed(new_charge: int)
 
 @export var equippable_to: Array[Slot]
+var can_do_actions : bool = true
 
-signal charge_changed(new_charge : int)
-# signal attached
-# signal detached
+@export var hold_time_curve: Curve
+var held_time_ticks :int  = 0
 
+enum Slot { LEFT_ARM, RIGHT_ARM, LEGS, }
 
-enum Slot {
-	LEFT_ARM,
-	RIGHT_ARM,
-	LEGS,
-}
-
-func handle_actions() -> void:
-	return
+func handle_actions(input_name: String) -> void:
+	var t1 :float = int(hold_time_curve.sample(0.0)) if hold_time_curve else 0.0
+	var t2 :float = int(hold_time_curve.sample(0.33)) if hold_time_curve else 10.0
+	var t3 :float = int(hold_time_curve.sample(0.66)) if hold_time_curve else 35.0
+	var t4 :float = int(hold_time_curve.sample(1.0)) if hold_time_curve else 60.0
+	
+	if Input.is_action_pressed(input_name):
+		held_time_ticks += 1
+	elif Input.is_action_just_released(input_name):
+		if t1 <= held_time_ticks and held_time_ticks < t2:
+			action_light()
+		elif t2 <= held_time_ticks and held_time_ticks < t3:
+			action_medium()
+		elif t4 <= held_time_ticks:
+			action_heavy()
+		held_time_ticks = 0
 
 func action_light() -> void:
 	return
@@ -36,6 +48,17 @@ func attach() -> bool:
 func detach() -> bool:
 	return false
 
+func accept_charge_if_possible (additional_charge: int) -> int:
+	# return accepted charge as int
+	var accepted_charge : int = 0;
+	if charge == charge_capacity: return accepted_charge
+	
+	if additional_charge + charge > charge_capacity:
+		accepted_charge = charge_capacity - charge
+		charge = charge_capacity
+	
+	return accepted_charge
+
 func use_charge_if_possible (cost: int) -> bool:
 	if cost > charge: return false
 	else:
@@ -44,7 +67,7 @@ func use_charge_if_possible (cost: int) -> bool:
 		return true
 	
 func create_splatter(spread: float, amount: int, direction: Vector2, origin: Vector2, raycast_range: float = 15.0, ) -> void:
-	print("ATTEMPING TO SPLATTER!")
+	# print("ATTEMPING TO SPLATTER!")
 	var increment = spread / amount
 	for i in range(amount):
 		# its actually 2x spread raycasts sorry
@@ -65,7 +88,7 @@ func splatter_raycast(direction: Vector2, origin: Vector2, raycast_range: float 
 	if result.is_empty(): return false
 	
 	var collider = result.get("collider")
-	print("hit something: %s at %s" % [collider.name, result.get("position")])
+	# print("hit something: %s at %s" % [collider.name, result.get("position")])
 	if !(collider is TileMapLayer): return false
 	
 	var body_rid = result.get("rid")
@@ -115,11 +138,11 @@ func fire_physics_raycast(direction: Vector2, origin: Vector2, length: float, la
 	var raycast_to := origin + (normalized_dir * length)
 	var query = PhysicsRayQueryParameters2D.create(raycast_from, raycast_to, layer)
 	
-	print("in 'fire_physics_raycast', shot raycast from %s to %s, angle_deg: %s" % [
-		raycast_from, 
-		raycast_to, 
-		rad_to_deg((raycast_to-raycast_from).angle())
-		])
+	#print("in 'fire_physics_raycast', shot raycast from %s to %s, angle_deg: %s" % [
+	#	raycast_from, 
+	#	raycast_to, 
+	#	rad_to_deg((raycast_to-raycast_from).angle())
+	#	])
 	
 	return space_state.intersect_ray(query)
 	
